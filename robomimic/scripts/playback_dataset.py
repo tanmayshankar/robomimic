7a454f069cf2444c642ff3408b0a63d15c5ffedc
Tanmay Shankar <tanmay.shankar@gmail.com>
2023-08-11 15:28:05 -0400

    Update playback_dataset.py

--- robomimic/scripts/playback_dataset.py
+++ robomimic/scripts/playback_dataset.py
@@ -68,6 +68,8 @@ import robomimic.utils.env_utils as EnvUtils
 import robomimic.utils.file_utils as FileUtils
 from robomimic.envs.env_base import EnvBase, EnvType
 
+from IPython import embed
+
 
 # Define default cameras to use for each env type
 DEFAULT_CAMERAS = {
